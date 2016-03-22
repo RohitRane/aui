7e5cd4ebfad9787d074313a6694eeb3bf098b5a1
Rohit Rane <Rohit.Rane@happiestminds.com>
2016-03-22 15:09:21 +0530

    Modified the gulp/script.js file to support multiple angular app modules

---
+++ src/app/components/navbar/navbar.module.js
@@ -0,0 +1,8 @@
+/*
+Author : Rohit Rane
+*/
+
+import { NavbarDirective } from './navbar.directive';
+
+angular.module('aftermarket.navbar',[])
+.directive('acmeNavbar', NavbarDirective);
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -6,10 +6,9 @@ import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
-angular.module('aftermarket', ['ngResource', 'ui.router', 'ui.bootstrap', 'toastr'])
+angular.module('aftermarket', ['ngResource', 'ui.router', 'ui.bootstrap', 'toastr','aftermarket.navbar'])
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .config(config)
@@ -18,7 +17,6 @@ angular.module('aftermarket', ['ngResource', 'ui.router', 'ui.bootstrap', 'toast
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
-  .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
 
 //Main module.
