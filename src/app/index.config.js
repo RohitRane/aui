684307db73bfbc62aa3ec07a899936da49a459df
Rohit Rane <Rohit.Rane@happiestminds.com>
2016-04-24 23:54:25 +0530

    Finished the implementation of the popover

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,7 +1,8 @@
-export function config($logProvider) {
+export function config($logProvider, $uibTooltipProvider) {
     'ngInject';
     // Enable log
     $logProvider.debugEnabled(true);
+    //$uibTooltipProvider.setTriggers({ 'mouseenter': 'click' });
 
     // Set options third-party lib
     
