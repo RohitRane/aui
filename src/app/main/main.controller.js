f03534c041f403fff6d46897da096f2413f01a7e
Rohit Rane <Rohit.Rane@happiestminds.com>
2016-04-26 23:47:13 +0530

    fixed the upper width limit of the website to 1280 px

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,33 +1,52 @@
 export class MainController {
-    constructor($scope, $timeout, $log, $document) {
+    constructor($scope, $timeout, $log, $document, $window) {
         'ngInject';
         let vm = this;
-        vm.DI = ()=> ({$scope, $timeout, $log, $document});
+        vm.DI = () => ({ $scope, $timeout, $log, $document });
 
-        
-        $scope.$on("searchbarFocussed",function () {
+        vm.landscape = true;
+
+        $scope.$on("searchbarFocussed", function () {
             vm.addOpaqueOverlay();
         });
-        
-        $scope.$on("searchbarBlurred",function () {
+
+        $scope.$on("searchbarBlurred", function () {
             vm.putOverlay = false;
         });
+
+        if ($window.innerHeight > $window.innerWidth) {
+            vm.landscape = false;
+        }
+        /*angular.element($window).bind('resize', function () {
+            console.log("resized");
+            console.log($window.innerHeight);
+            console.log($window.innerWidth);
+            if ($window.innerHeight > $window.innerWidth) {
+                console.log("true");
+                vm.landscape = false;
+            }
+            else {
+                vm.landscape = true;
+            }
+            $scope.$apply();
+        })*/
+
     }
 
-      
+
     addOpaqueOverlay() {
         //var mc = angular.element(('#main-content')).css("color");
         var vm = this;
         let {$timeout, $log, $document} = vm.DI();
         //this.DI.timeout(function () {
-            var mc =$document[0].getElementById('main-content');
-            var mcHeight = mc.offsetHeight;
-            vm.putOverlay = true;
-            $timeout(function () {
-                var overlay = angular.element($document[0].getElementById('overlay'));
-                $log.debug(overlay);
-                overlay.css("height", mcHeight+'px');
-            });
+        var mc = $document[0].getElementById('main-content');
+        var mcHeight = mc.offsetHeight;
+        vm.putOverlay = true;
+        $timeout(function () {
+            var overlay = angular.element($document[0].getElementById('overlay'));
+            $log.debug(overlay);
+            overlay.css("height", mcHeight + 'px');
+        });
 
         //});
 
