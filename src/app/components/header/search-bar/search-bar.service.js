f3c93084aa3baf3404248bd31f3581c9c655c689
Rohit Rane <Rohit.Rane@happiestminds.com>
2016-05-06 15:56:48 +0530

    Created the session storage for search

--- src/app/components/header/search-bar/search-bar.service.js
+++ src/app/components/header/search-bar/search-bar.service.js
@@ -1,57 +1,79 @@
-export class SearchBarService{
+export class SearchBarService {
     
     /*constructor(srchStr) {
         this{._srchStr = srchStr;
     }*/
-    constructor(){
+    constructor() {
         this._filters = [{}];
+        this._retrieveFromSession();
     }
-     
+
     get srchStr() {
         return this._srchStr;
     }
- 
-    set srchStr(newSrchStr){
-        if(newSrchStr){ 
+
+    set srchStr(newSrchStr) {
+        if (newSrchStr) {
             this._srchStr = newSrchStr;
+            this._saveToSession();
         }
     }
-    
+
     get productLine() {
         return this._productLine;
     }
- 
-    set productLine(newSrchStr){
-        if(newSrchStr){ 
+
+    set productLine(newSrchStr) {
+        if (newSrchStr) {
             this._productLine = newSrchStr;
+            this._saveToSession();
         }
     }
-    
+
     get productCategory() {
         return this._productCategory;
     }
- 
-    set productCategory(newProductCategory){
-            this._productCategory = newProductCategory;
+
+    set productCategory(newProductCategory) {
+        this._productCategory = newProductCategory;
+        this._saveToSession();
     }
-    
-     get typeId() {
+
+    get typeId() {
         return this._typeId;
     }
- 
-    set typeId(newTypeId){
-        if(newTypeId){ 
+
+    set typeId(newTypeId) {
+        if (newTypeId) {
             this._typeId = newTypeId;
+            this._saveToSession();
         }
     }
-    
+
     get filters() {
         return this._filters;
     }
- 
-    set filters(newfilters){
-        if(newfilters){ 
+
+    set filters(newfilters) {
+        if (newfilters) {
             this._filters = newfilters;
+            this._saveToSession();
         }
+    }
+
+    _saveToSession() {
+        sessionStorage.srchStr = this._srchStr;
+        sessionStorage.productLine = this._productLine;
+        sessionStorage.productCategory = this._productCategory;
+        sessionStorage.typeId = this._typeId;
+        sessionStorage.filters = this._filters;
+    }
+
+    _retrieveFromSession() {
+        this._srchStr = sessionStorage.srchStr;
+        this._productLine = sessionStorage.productLine;
+        this._productCategory = sessionStorage.productCategory;
+        this._typeId = sessionStorage.typeId;
+        this._filters = sessionStorage.filters;
     }
 }
