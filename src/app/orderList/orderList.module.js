45570f35f3d44fb7c1bfdecba311d89f785242df
Shaifali Jaiswal <Shaifali.Jaiswal@happiestminds.com>
2016-05-18 14:48:35 +0530

    remove the error from orderlistmodule.js

--- src/app/orderList/orderList.module.js
+++ src/app/orderList/orderList.module.js
@@ -2,7 +2,7 @@
 import{ OrderListController } from './orderList.controller';
 import{ routeConfig } from './orderList.route'
 
-angular.module('aftermarket.orderList', ['aftermarket.core'])
+angular.module('aftermarket.orderList', [])
 	.config(routeConfig)
 	.controller('OrderListController', OrderListController);
 
