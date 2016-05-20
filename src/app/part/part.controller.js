1795c26a836afcc1be0ef706cdb55bd46ae085be
Rohit Rane <Rohit.Rane@happiestminds.com>
2016-05-20 15:36:19 +0530

    Integrated comp table for automotive part info page with API data

--- src/app/part/part.controller.js
+++ src/app/part/part.controller.js
@@ -27,6 +27,58 @@ export class PartController {
             width: 80
         };
 
+        vm.hideForNow = true;
+
+    }
+
+    getPart() {
+        let vm = this;
+        let {$log, $stateParams, $scope, SearchBarService, dataServices} = vm.DI();
+        $log.debug("part no :", $stateParams);
+        vm.productLine = SearchBarService.productLine;
+        $log.debug("state type :", $stateParams.type);
+        $scope.$emit("showLoading", true);
+        if ($stateParams.type === "partnum") {
+            $log.debug("searching by part num.");
+            dataServices.partByPartNum($stateParams.val).then(function (response) {
+                $log.debug("Response in Controller :", response);
+                vm.partData = response;
+                vm._createCompatibilityTab();
+                $scope.$emit("showLoading", false);
+            }, function (error) {
+                $scope.$emit("showLoading", false);
+            });
+        } else {
+            dataServices.part($stateParams.id).then(function (response) {
+                $log.debug("Response in Controller :", response);
+                vm.partData = response;
+                switch (vm.partData.categories[2]) {
+                    case 'Flanges': console.log("It's a flanges"); vm.partData.imageUrl = "/assets/images/flange.png"; break;
+                    case 'Universal Joints': console.log("It's a Universal Jt"); vm.partData.imageUrl = "/assets/images/u-joint.jpg"; break;
+                    case 'Flange Yoke': console.log("It's a Universal Jt"); vm.partData.imageUrl = "/assets/images/flange_yoke.jpg"; break;
+                    case 'Ring and Pinions': console.log("It's a Universal Jt"); vm.partData.imageUrl = "/assets/images/rangeNpinion.jpg"; break;
+                    default: vm.partData.imageUrl = "http://placehold.it/160x160/dbdbdb/0099CC/?text=NO+IMAGE";
+                };
+                vm._createCompatibilityTab();
+                $scope.$emit("showLoading", false);
+            }, function (error) {
+                $scope.$emit("showLoading", false);
+            });
+        }
+    }
+
+    hasSpecification(attrs) {
+        if (attrs) {
+            let attrsArr = Object.keys(attrs);
+            if (attrsArr.length === 0) {
+                return false;
+            }
+            else return true
+        } else return false;
+    }
+
+    _createCompatibilityTab() {
+        let vm = this;
         vm.ymmCompatibility = [
             {
                 'year': 2007,
@@ -57,55 +109,10 @@ export class PartController {
 
         vm.ymmCompatibilityTab1 = [], vm.ymmCompatibilityTab2 = [];
 
-        angular.forEach(vm.ymmCompatibility, (ymm, index, compArr) => {
+        console.log("part dta :", vm.partData.apps);
+
+        angular.forEach(vm.partData.apps, (ymm, index, compArr) => {
             (index < Math.ceil(compArr.length / 2)) ? vm.ymmCompatibilityTab1.push(ymm) : vm.ymmCompatibilityTab2.push(ymm);
         });
-
-        vm.hideForNow = true;
-
-    }
-
-    getPart() {
-        let vm = this;
-        let {$log, $stateParams, $scope, SearchBarService, dataServices} = vm.DI();
-        $log.debug("part no :", $stateParams);
-        vm.productLine = SearchBarService.productLine;
-        $log.debug("state type :", $stateParams.type);
-        $scope.$emit("showLoading", true);
-        if ($stateParams.type === "partnum") {
-            $log.debug("searching by part num.");
-            dataServices.partByPartNum($stateParams.val).then(function (response) {
-                $log.debug("Response in Controller :", response);
-                vm.partData = response;
-                $scope.$emit("showLoading", false);
-            }, function (error) {
-                $scope.$emit("showLoading", false);
-            });
-        } else {
-            dataServices.part($stateParams.id).then(function (response) {
-                $log.debug("Response in Controller :", response);
-                vm.partData = response;
-                switch (vm.partData.categories[2]) {
-                    case 'Flanges': console.log("It's a flanges"); vm.partData.imageUrl = "/assets/images/flange.png"; break;
-                    case 'Universal Joints': console.log("It's a Universal Jt"); vm.partData.imageUrl = "/assets/images/u-joint.jpg"; break;
-                    case 'Flange Yoke': console.log("It's a Universal Jt"); vm.partData.imageUrl = "/assets/images/flange_yoke.jpg"; break;
-                    case 'Ring and Pinions': console.log("It's a Universal Jt"); vm.partData.imageUrl = "/assets/images/rangeNpinion.jpg"; break;
-                    default: vm.partData.imageUrl = "http://placehold.it/160x160/dbdbdb/0099CC/?text=NO+IMAGE";
-                };
-                $scope.$emit("showLoading", false);
-            }, function (error) {
-                $scope.$emit("showLoading", false);
-            });
-        }
-    }
-
-    hasSpecification(attrs) {
-        if (attrs) {
-            let attrsArr = Object.keys(attrs);
-            if (attrsArr.length === 0) {
-                return false;
-            }
-            else return true
-        } else return false;
     }
 }
